57c19472a47685cefe130ecc281a5b7b98fcb67b
[softxjl] <[softxjl@126.com]>
2019-03-20 19:01:49 +0800

    11

--- FCode/FCode.Api/api/IndexController.cs
+++ FCode/FCode.Api/api/IndexController.cs
@@ -1,4 +1,5 @@
-﻿using FCode.Utility;
+﻿using FCode.Entity;
+using FCode.Utility;
 using FCode.VModel;
 using System;
 using System.Collections.Generic;
@@ -16,7 +17,11 @@ namespace FCode.Api
         {
             var result = new ReturnResult();
             var token = this.GetToken();
-            if (token != null) result.data = "访问成功！";
+            if (token == null) result.msg = "身份无效！";
+            var db = new FCodeDbContext();
+            //db.Database.CreateIfNotExists();
+            db.Users.Add(new User { Name = "Jace夏将龙", Age = 30, Sex = true });
+            result.data = db.SaveChanges();
             return result;
         }
     }
