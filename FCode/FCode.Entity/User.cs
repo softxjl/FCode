6a6d5ad399a85c54437c9102dc5d0c26afb18eb4
[softxjl] <[softxjl@126.com]>
2019-03-25 17:28:30 +0800

    111

---
+++ FCode/FCode.Entity/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCode.Entity
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public bool Sex { get; set; }
+    }
+
+    public class Test
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId{ get; set; }
+
+        public string TestName { get; set; }
+    }
+}
